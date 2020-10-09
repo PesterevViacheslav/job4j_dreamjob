80895725cf735d5378adf4c270a2ed59488e80fe
ViacheslavPesterev <pesterevvv@gmail.com>
2020-10-09 18:29:27 +0300

    Middle. Блок 2. Servlet JSP. 2. JSP. 2. Scriplet. Динамическая таблица.[#282978]

---
+++ src/main/java/ru/job4j/dream/model/Post.java
@@ -0,0 +1,106 @@
+package ru.job4j.dream.model;
+import java.util.Date;
+import java.util.Objects;
+/**
+ * Class Post - Модель данных. Решение задач уровня Middle. Части 012. Servlet JSP.
+ * 2. JSP 2. Scriplet. Динамическая таблица.[#282978]
+ *
+ * @author Viacheslav Pesterev (pesterevvv@gmail.com)
+ * @since 09.10.2020
+ * @version 1
+ */
+public class Post {
+    private int id;
+    private String name;
+    private String description;
+    private Date created;
+
+    /**
+     * Method Post. Конструктор
+     * @param id ID
+     * @param name Название
+     * @param description Описание
+     * @param created Дата регистрации
+     */
+    public Post(int id, String name, String description, Date created) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.created = created;
+    }
+    /**
+     * Method getId. Получение ID
+     * @return Id
+     */
+    public int getId() {
+        return id;
+    }
+    /**
+     * Method setId. Изменение ID
+     * @param id ID
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+    /**
+     * Method getName. Получение названия
+     * @return Название
+     */
+    public String getName() {
+        return name;
+    }
+    /**
+     * Method setName. Изменение названия
+     * @param name Название
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+    /**
+     * Method getDescription. Получение описания
+     * @return Описание
+     */
+    public String getDescription() {
+        return description;
+    }
+    /**
+     * Method setDescription. Изменение описания
+     * @param description Описание
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    /**
+     * Method getCreated. Получение даты создания
+     * @return Дата создания
+     */
+    public Date getCreated() {
+        return created;
+    }
+    /**
+     * Method setCreated. Изменение даты создания
+     * @param created Дата создания
+     */
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+    @Override
+    public String toString() {
+        return "Post{" + "id=" + id + ", name='" + name + '\'' + ", description='" + description + '\'' + ", created=" + created +
+                '}';
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return id == post.id &&
+                Objects.equals(name, post.name) &&
+                Objects.equals(description, post.description) &&
+                Objects.equals(created, post.created);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/ru/job4j/dream/store/Store.java
@@ -0,0 +1,40 @@
+package ru.job4j.dream.store;
+import ru.job4j.dream.model.Post;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+/**
+ * Class Store - Хранилище данных. Решение задач уровня Middle. Части 012. Servlet JSP.
+ * 2. JSP 2. Scriplet. Динамическая таблица.[#282978]
+ *
+ * @author Viacheslav Pesterev (pesterevvv@gmail.com)
+ * @since 09.10.2020
+ * @version 1
+ */
+public class Store {
+    private static final Store INST = new Store();
+    private Map<Integer, Post> posts = new ConcurrentHashMap<>();
+    /**
+     * Method Store. Конструктор
+     */
+    private Store() {
+        posts.put(1, new Post(1, "Junior Java Job", "Junior", new Date()));
+        posts.put(2, new Post(2, "Middle Java Job", "Middle", new Date()));
+        posts.put(3, new Post(3, "Senior Java Job", "Senior", new Date()));
+    }
+    /**
+     * Method instOf. Получение экземпляра
+     * @return Экземпляр
+     */
+    public static Store instOf() {
+        return INST;
+    }
+    /**
+     * Method findAll. Получение элементов хранилища
+     * @return Коллекция элементов
+     */
+    public Collection<Post> findAll() {
+        return posts.values();
+    }
+}
