dd2c8fde0e51178556a58874ca7c07c4dc531216
ViacheslavPesterev <pesterevvv@gmail.com>
2021-01-24 17:02:54 +0300

    Middle. Блок 2. Servlet JSP. 8. JS, JQuery, Ajax. 4.1. JSON формат.[282966] (доработка)

--- src/main/java/ru/job4j/dream/servlet/JsonController.java
+++ src/main/java/ru/job4j/dream/servlet/JsonController.java
@@ -1,19 +1,31 @@
 package ru.job4j.dream.servlet;
+import com.google.gson.Gson;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Enumeration;
 
 public class JsonController extends HttpServlet {
+    class Response {
+        protected String text;
+    }
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Gson gson = new Gson();
+        Enumeration en = req.getParameterNames();
+        Response response = null;
+        while (en.hasMoreElements()) {
+            response = gson.fromJson((String) en.nextElement(), Response.class);
+        }
         resp.setContentType("text/json");
         resp.setCharacterEncoding("UTF-8");
-        String name = req.getParameter("name");
         PrintWriter writer = new PrintWriter(resp.getOutputStream());
-        writer.println("{\"name\" : \"" + name + "\", \"text\" : \" Nice to meet you, \"}");
+        writer.println("{\"name\" : \"" + response.text + "\", \"text\" : \" Nice to meet you, \"}");
         writer.flush();
     }
+
 }
